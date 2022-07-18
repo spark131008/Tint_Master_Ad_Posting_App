c10c4c538c4217d4d59054e019c24673ae7a2b1b
Sundoo Park <spark@MacBook-Pro.local>
2022-07-17 23:15:01 -0400

    updated build and deploy code

--- lambda_code/GTech_auto_posting_app-lambda.py
+++ lambda_code/GTech_auto_posting_app-lambda.py
@@ -34,14 +34,14 @@ def _init_bin(executable_name):
     if not os.path.exists(BIN_DIR):
         print("Creating bin folder")
         os.makedirs(BIN_DIR)
-    print("Copying binaries for " + executable_name + " in /tmp/bin")
-    currfile = os.path.join(CURR_BIN_DIR, executable_name)
-    newfile = os.path.join(BIN_DIR, executable_name)
-    shutil.copy2(currfile, newfile)
-    print("Giving new binaries permissions for lambda")
-    os.chmod(newfile, 0o775)
-    elapsed = time.clock() - start
-    print(executable_name + " ready in " + str(elapsed) + "s.")
+    # print("Copying binaries for " + executable_name + " in /tmp/bin")
+    # currfile = os.path.join(CURR_BIN_DIR, executable_name)
+    # newfile = os.path.join(BIN_DIR, executable_name)
+    # shutil.copy2(currfile, newfile)
+    # print("Giving new binaries permissions for lambda")
+    # os.chmod(newfile, 0o775)
+    # elapsed = time.clock() - start
+    # print(executable_name + " ready in " + str(elapsed) + "s.")
 
 def openURL():
     print('start')
@@ -75,7 +75,7 @@ def openURL():
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--window-size=1280x1696')
+        chrome_options.add_argument('--window-size={}x{}'.format(1280, 1024))
         chrome_options.add_argument('--user-data-dir=/tmp/user-data')
         chrome_options.add_argument('--hide-scrollbars')
         chrome_options.add_argument('--enable-logging')
@@ -90,7 +90,8 @@ def openURL():
             'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
         # chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
         chrome_options.binary_location = "/tmp/bin/headless-chromium"
-        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/tmp/bin/chromedriver")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        # driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/tmp/bin/chromedriver")
         driver.get(url_gtech_1)
         print('open web browser')
 
