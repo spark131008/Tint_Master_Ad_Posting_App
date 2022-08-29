2221530e4aa8e740b66d138785a0bb9156279fcd
Sundoo Park <spark@MacBook-Pro.local>
2022-08-29 01:42:08 -0400

    driver option updated

--- lambda_code/gtech-ad-posting-app-lambda.py
+++ lambda_code/gtech-ad-posting-app-lambda.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from selenium import webdriver
 from tempfile import mkdtemp
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 id_gtech = os.environ.get('id_gtech')
@@ -66,25 +67,27 @@ def openURL():
 <p class="p1" style="font-size:12px;line-height:0.9;font-family:'Helvetica Neue';"><span style="font-size:48px;">678-731-7177</span></p>
         '''
 
-        options = webdriver.ChromeOptions()
-        options.binary_location = '/opt/chrome/chrome'
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1280x1696")
-        options.add_argument("--single-process")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-dev-tools")
-        options.add_argument("--no-zygote")
-        options.add_argument(f"--user-data-dir={mkdtemp()}")
-        options.add_argument(f"--data-path={mkdtemp()}")
-        options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        options.add_argument("--remote-debugging-port=9222")
+        # options = webdriver.ChromeOptions()
+        # options.binary_location = '/opt/chrome/chrome'
+        # options.add_argument('--headless')
+        # options.add_argument('--no-sandbox')
+        # options.add_argument("--disable-gpu")
+        # options.add_argument("--window-size=1280x1696")
+        # options.add_argument("--single-process")
+        # options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument("--disable-dev-tools")
+        # options.add_argument("--no-zygote")
+        # options.add_argument(f"--user-data-dir={mkdtemp()}")
+        # options.add_argument(f"--data-path={mkdtemp()}")
+        # options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+        # options.add_argument("--remote-debugging-port=9222")
+        options = Options()
+        options.headless = True
         driver = webdriver.Chrome("/opt/chromedriver",
                                   options=options)
 
         driver.get(url_gtech_1)
-        print('open web browser')
+        print('opened web browser')
 
         element_id = driver.find_element(by=By.ID, value="login_id")
         element_id.send_keys(id_gtech)
