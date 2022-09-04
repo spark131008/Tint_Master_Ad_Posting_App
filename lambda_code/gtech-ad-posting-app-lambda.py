b65109ec2179d4845c7058164b65eacb33a72651
Sundoo Park <spark@MacBook-Pro.local>
2022-09-04 16:35:14 -0400

    updated library used

--- lambda_code/gtech-ad-posting-app-lambda.py
+++ lambda_code/gtech-ad-posting-app-lambda.py
@@ -59,21 +59,20 @@ def download_ad_image():
     print("download_dir:", os.listdir(download_dir))
 
 def clean_tmp_folder():
-    os.removedirs(download_dir)
-    print("download_dir:", os.listdir(download_dir))
+    shutil.rmtree(download_dir)
+    print("tmp_dir:", os.listdir("/tmp"))
 
 def openURL():
     print('start')
-    try:
-        content = '''
+    content = '''
 <p class="li1" style="font-size:12px;line-height:0.5;font-family:'Helvetica Neue';"><span style="font-size:26px;">
 <span style="color:rgb(241,196,15);"><span style="font-size:36px;"><span style="background-color:rgb(0,0,204);">틴트마스터 블랙박스 스페셜</span></span></span> <br><br>
 (최신형) 아이나비 QXD7000  <br><br>
 (최신형) 아이나비 X1000  <br><br>
 (최신형) 아이나비 X700  <br><br>
 (최신형) 아이나비 Z3000  <br><br>
-             아이나비 A500  <br><br>
-             아이나비 F200 Pro <br><br>
+         아이나비 A500  <br><br>
+         아이나비 F200 Pro <br><br>
 <span style="font-size:20px;"><span style="line-height:1.5;">**블랙박스는 반드시 전문가에게 맡기셔야 합니다. 블랙박스 설치 시, 배선작업을 잘못할 경우 심각한 전기문제를 초래할 수 있습니다. 틴트 마스터에서는 30년 경험의 전문가가 책임지고 설치해 드립니다. 블랙박스 설치전, 사전상담은 필수입니다.**</span></span><br>
 </span></p>
 
@@ -88,66 +87,63 @@ def openURL():
 <p class="p3" style="font-size:12px;line-height:0.5;font-family:'Helvetica Neue';color:rgb(220,161,13);"><span style="font-size:26px;"><a href="mailto:tintmasteratl@gmail.com">tintmasteratl@gmail.com</a></span></p>
 
 <p class="p1" style="font-size:12px;line-height:0.9;font-family:'Helvetica Neue';"><span style="font-size:48px;">678-731-7177</span></p>
-        '''
+    '''
 
-        options = webdriver.ChromeOptions()
-        options.binary_location = '/opt/chrome/chrome'
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1280x1696")
-        options.add_argument("--single-process")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-dev-tools")
-        options.add_argument("--no-zygote")
-        options.add_argument(f"--user-data-dir={mkdtemp()}")
-        options.add_argument(f"--data-path={mkdtemp()}")
-        options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        options.add_argument("--remote-debugging-port=9222")
-        driver = webdriver.Chrome("/opt/chromedriver",
-                                  options=options)
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome("/opt/chromedriver",
+                              options=options)
 
-        driver.get(url_gtech_1)
-        print('opened web browser')
+    driver.get(url_gtech_1)
+    print('opened web browser')
 
-        element_id = driver.find_element(by=By.ID, value="login_id")
-        element_id.send_keys(login_id)
-        element_pwd = driver.find_element(by=By.ID, value="login_pw")
-        element_pwd.send_keys(login_pw)
-        print('entered id/pwd')
+    element_id = driver.find_element(by=By.ID, value="login_id")
+    element_id.send_keys(login_id)
+    element_pwd = driver.find_element(by=By.ID, value="login_pw")
+    element_pwd.send_keys(login_pw)
+    print('entered id/pwd')
 
-        element_click1 = driver.find_element(by=By.CLASS_NAME, value='btn_submit')
-        element_click1.click()
-        print('login completed')
+    element_click1 = driver.find_element(by=By.CLASS_NAME, value='btn_submit')
+    element_click1.click()
+    print('login completed')
 
-        driver.get(url_gtech_2)
-        print('entering the advertising page')
-        element_click2 = driver.find_element(by=By.CLASS_NAME, value="chk_box")
-        element_click2.click()
-        print('clicked html')
+    driver.get(url_gtech_2)
+    print('entering the advertising page')
+    element_click2 = driver.find_element(by=By.CLASS_NAME, value="chk_box")
+    element_click2.click()
+    print('clicked html')
 
-        driver.switch_to.alert.accept()
-        print('alert accepted')
+    driver.switch_to.alert.accept()
+    print('alert accepted')
 
-        # Subject typed
-        element_subject = driver.find_element(by=By.ID, value='wr_subject')
-        element_subject.send_keys('◆ 자동차 썬팅/블랙박스 전문점 틴트 마스터, 아이나비 블랙박스 특별세일!!! ◆')
-        print('subject typed')
+    # Subject typed
+    element_subject = driver.find_element(by=By.ID, value='wr_subject')
+    element_subject.send_keys('◆ 자동차 썬팅/블랙박스 전문점 틴트 마스터, 아이나비 블랙박스 특별세일!!! ◆')
+    print('subject typed')
 
-        # Content typed
-        element_subject = driver.find_element(by=By.ID, value='wr_content')
-        element_subject.send_keys(content)
-        print('content typed')
+    # Content typed
+    element_subject = driver.find_element(by=By.ID, value='wr_content')
+    element_subject.send_keys(content)
+    print('content typed')
 
-        # photo attached
-        element_attachment = driver.find_element(by=By.ID, value="bf_file_1")
-        # element_attachment.send_keys(ad_pic_object)
-        print("Photo attached")
+    # photo attached
+    element_attachment = driver.find_element(by=By.ID, value="bf_file_1")
+    # element_attachment.send_keys(ad_pic_object)
+    print("Photo attached")
 
-        # submit
-        element_submit_btn = driver.find_element(by=By.ID, value='btn_submit')
-        element_submit_btn.click()
-        print('AD post uploaded')
-
-    except Exception as e:
-        print(e)
+    # submit
+    element_submit_btn = driver.find_element(by=By.ID, value='btn_submit')
+    element_submit_btn.click()
+    print('AD post uploaded')
